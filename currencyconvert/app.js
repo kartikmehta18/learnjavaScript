853b803ec621259b37dc5b74cbe048bd661451d7
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-04-30 16:51:51 +0530

    js

--- currencyconvert/app.js
+++ currencyconvert/app.js
@@ -1,12 +1,12 @@
 const BASE_URL  =
 "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
+// "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@/latest/v1/currencies";
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector(" form button");
 
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
-
+const msg = document.querySelector(".msg");
 for (let select of dropdowns){
     for (currCode in countryList) {
        let newOption = document.createElement("option");
@@ -45,7 +45,10 @@ btn.addEventListener("click",async(evt)=>{
 //    console.log(toCurr.value,fromCurr.value);
     const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
    let response =await fetch(URL);
-//    let data =await response.json(); 
-  console.log(response);
+   let data =await response.json(); 
+   let rate = data[toCurr.value.toLowerCase()];
+   let finalAm = amtVal*rate;
+   msg.innerText =`${amtVal} ${fromCurr.value}= ${finalAm}${toCurr.value}` ;
+
 
 });
